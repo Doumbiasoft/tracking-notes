babc10239df8b2f9bcca989068663090438ee12f
Mouhamed Doumbia <doumbiasoft@gmail.com>
2025-08-15 14:31:51 -0400

    added implemented registration events

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,7 @@
+const regexOfNoSpecialCharacterOrWhiteSpaceInText = /^[a-zA-Z0-9]+$/;
+const regexOfCheckPassword =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9])(?!.*[Pp][Aa][Ss][Ss][Ww][Oo][Rr][Dd]).{12,}$/;
+
 const getUsers = () => {
   return JSON.parse(localStorage.getItem("users")) || [];
 };
@@ -18,3 +22,23 @@ const logout = () => {
   localStorage.removeItem("currentUser");
   location.href = "../index.html";
 };
+
+const $registerForm = document.getElementById("registerForm");
+if ($registerForm) {
+  registerForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const $username = document.getElementById("regUsername").value.trim();
+    const $password = document.getElementById("regPassword").value.trim();
+
+    const users = getUsers();
+    if (users.find((u) => u.username === $username)) {
+      alert("Username already taken!");
+      return;
+    }
+
+    users.push({ $username, $password, notes: [] });
+    saveUsers(users);
+    alert("Registration successful! Please log in.");
+    location.href = "../index.html";
+  });
+}
